c40d33bee016f0819e73efe2e5cdbf4ed5d01a35
Abhishek Sharma <contact.abhishek.es@gmail.com>
2020-10-17 01:34:01 +0530

    added firebase config and created apps for social sign on

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, HeaderBackButton } from '@react-navigation/stack';
-import {BackHandler, Image} from 'react-native';
+import {Image} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import SplashScreen from "./src/screens/splash";
@@ -12,7 +12,7 @@ const Stack = createStackNavigator();
 
 const HomeIcon = <Icon name="home" size={30} color="#4C4C4C" />;
 
-function App() {
+export default function App() {
   useEffect(() => {
     (async () => {
       const { status } = await Calendar.requestCalendarPermissionsAsync();
@@ -56,5 +56,3 @@ function LogoTitle() {
     />
   );
 }
-
-export default App;
